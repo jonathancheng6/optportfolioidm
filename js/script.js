bd81b398228ca5365f3eed34392f4653bf1bcaae
jonathancheng6 <jonathancheng@Jonathans-MacBook-Pro-2.local>
2015-11-30 12:31:18 -0500

    script 2
    additional text

--- js/script.js
+++ js/script.js
@@ -168,7 +168,7 @@ $(document).ready(function(){
 	setTimeout( function(){
 		if($("#site-type").length) {
         $(".typewrite span").typed({
-            strings: ["show case ", "projects ", "portfolio"],
+            strings: ["show case ", "projects ", "portfolio "],
             typeSpeed: 100,
             backDelay: 500,
             loop: false,
